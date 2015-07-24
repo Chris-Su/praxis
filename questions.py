8de61c809f973a1a19fae5d7329ce5f1e06b4c97
testing123 <c.s@live.ca>
2015-07-24 17:43:44 -0400

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,22 +1,18 @@
 from willie import module
 
-response = {'yes': 0 , 'no': 0}
+questions = []
 
 @module.commands('ask')
-@module.example('.ask how much is this test worth?')
+@module.example('.ask [question] | .ask questionlist | ')
 def vote(bot, trigger):
     question = triggger[4:]
     if not question:
         bot.say("you have not asked a question")
         return
-    elif word == 'results':
-        for i in response:
-            bot.say(i + ' ' + str(response[i]))
-        return
-    elif word in response:
-        response[word] += 1
-        bot.reply("you have voted " + word)
+    elif question == questionlist:
+        for i in questions
+        bot.say(i)
         return
     else:
-        bot.say(word + "is not a valid response")
+        bot.say("error")
     
