e2f2f11a53a39971b07a6d0d61e7cd779d688d9c
testing123 <c.s@live.ca>
2015-07-27 16:34:54 -0400

    Update questions.py

--- questions.py
+++ questions.py
@@ -3,16 +3,17 @@ from willie import module
 questions = []
 
 @module.commands('ask')
-@module.example('.ask [question] | .ask questionlist | ')
+@module.example('.ask [question] | .ask questionlist')
 def vote(bot, trigger):
     question = triggger[4:]
     if not question:
         bot.say("you have not asked a question")
-        return
     elif question == questionlist:
         for i in questions
         bot.say(i)
-        return
+    elif question.length() < 100:
+        questions.append(question)
     else:
         bot.say("error")
+    return
     
