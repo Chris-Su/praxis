76b6760167d531f17441860d1332e8dcffab19d5
testing123 <c.s@live.ca>
2015-07-28 11:59:18 -0400

    Update questions.py

--- questions.py
+++ questions.py
@@ -3,11 +3,15 @@ from willie import module
 questions = []
 
 @module.commands('ask')
-@module.example('.ask [question] | .ask questionlist')
+@module.example('.ask [question] | .ask questionlist | .ask answer [question #]')
 def vote(bot, trigger):
     question = triggger[4:]
     if not question:
         bot.say("you have not asked a question")
+    elif trigger.group(2)
+        if trigger.group(3).isdigit()
+            bot.say('question ' + trigger.group(3) + ' has been answered')
+            del questions[int(trigger.group(3)) - 1]
     elif question == questionlist:
         for i in questions
         bot.say(i)
